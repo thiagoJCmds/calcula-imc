4560156143c969a20dac3cd8a9e3b55897711a63
Thiago Manoel <thiagomds03@gmail.com>
2024-01-08 15:17:20 -0300

    finalizado

--- calcula-imc/src/componentes/Form.jsx
+++ calcula-imc/src/componentes/Form.jsx
@@ -1,62 +1,84 @@
-import { useState } from "react";
+import { useState , useRef, useEffect } from "react";
 import './Form.css';
 
 function Form () {
     
     
-    const [peso, setPeso] = useState('');
-    const [altura, setAltura] = useState('');
+    const [peso, setPeso] = useState();
+    const [altura, setAltura] = useState();
+    const [cal, setCal] = useState();
+    const clear = useRef("")
+    const limpa = useRef("")
+ 
     
-    
-    const calcularImc = () =>{
+    function calcularImc () {
         const al = Math.pow(altura, 2);
-        const soma = peso / al
-        console.log(soma)
-        
-            if (soma <= 18){
+        setCal(peso / al);
+       
+    }   
+    
+        const resultado = () => {
+
+            if (cal <= 18){
                 return (
-                    <p>Baixo peso</p>
+                    <p id="p1">abaixo do peso</p>
                 );
             }
-            else if (soma <= 25){
+            else if (cal <= 25){
                 return(
-                <p>peso normal</p>
+                <p id="p2">peso normal</p>
                 );
             }
-            else if (soma <= 30){
-                return( <p>Sobrepeso</p>
-                 
+            else if (cal <= 30){
+                return( <p id="p3">Sobrepeso</p>
+                
                 );
             }   
-            else if (soma) {
-                 return (
-                     <p>Obesidade</p>
-                 );
-             }
-       
-    }    
-             
-               
-        
+            else if (cal){
+                return (
+                    <p id="p4">Obesidade</p>
+                )
+            }
           
-            
+        
+             
+        }
 
-    const stopReload = (e)=> {
-        e.preventDefault
-    }
+        const reset = ()=>{
+            clear.current.value = ""
+            limpa.current.value = ""
+            setCal()
+        }
+
+        
+            
+        
+        const stopReload = (e)=> {
+            e.preventDefault()
+
+        }
+    
     
 
     return (
         <div>
             <form onSubmit={stopReload}>
-                <label htmlFor="altura">Altura</label>
-                <input type="text" onChange={(evento) => setAltura(evento.target.value)}/>
-                <label htmlFor="peso">Peso</label>
-                <input type="text" onChange={(evento) => setPeso(evento.target.value)}/>
-                {calcularImc()}
-                <div className="container">
-                <button type="button">Caucular IMC</button>
+            <h1>Calcule o indice de massa corporal</h1>
+                <label  htmlFor="altura">Altura</label>
+                <div>
+                <input placeholder="exemplo 1.70" ref={clear} type="text" onChange={(evento) => setAltura(evento.target.value)}/>
                 </div>
+                <label htmlFor="peso">Peso</label>
+                <div>
+                <input placeholder="exemplo 80" ref={limpa} type="text" onChange={(evento) => setPeso(evento.target.value)}/>
+                </div>
+                <div className="container">
+                <button className="b1" onClick={calcularImc} type="button">Caucular IMC</button>
+                <button onClick={reset} type="button">Limpar</button>
+                </div>
+                <div>
+                {resultado()}    
+                </div>     
             </form>
         </div>
 
